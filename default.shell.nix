ed93dda6341fc0ba499741b658cb4918adc0ac85
nokx <info@nokx.ch>
2021-03-14 00:24:25 +0100

    very strange abi error for clang previously

--- default.shell.nix
+++ default.shell.nix
@@ -2,19 +2,36 @@
 
 with (import <nixpkgs> { });
 
-let stdenv = if clangSupport then clangStdenv else gccStdenv;
+
+
+let
+# clangStdenv.override (x: {
+#   cc = x.cc.override (_: {
+#     libcxx = llvmPackages.libcxx;
+#   });
+# }))
+
+stdenv = if clangSupport then clangStdenv else gccStdenv;
 in (mkShell.override { inherit stdenv; }) rec {
 
-  nativeBuildInputs = [ cmake ninja gnumake gdb catch2 ] # valgrind pkgconfig
+  nativeBuildInputs = [  gnumake cmake ninja meson gdb catch2 ] # valgrind pkgconfig
     ++ lib.optional (!clangSupport) lcov; # [ sssd cacert ]
 
   buildInputs = [
+    # binutils-unwrapped
+    # llvm
+    # protobuf
+    # stdenv.cc.cc.lib
+    # libcxxStdenv
+    # libcxx
+    # libcxxabi
     # blas hdf5 tbb
     # tbb
     boost
     # eigen
   ] # ++ lib.optionals cudaSupport [ cudatoolkit.cc ] # cudatoolkit nvidia_x11
-    ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames;
+    ++ lib.optional stdenv.hostPlatform.isDarwin fixDarwinDylibNames
+    ;
 
   shellHook = ''
     export PATH=/nix/var/nix/profiles/per-user/$USER/tools-dev/bin/:$PATH
